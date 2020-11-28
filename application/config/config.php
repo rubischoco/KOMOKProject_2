f336f45adbb9e8142d5666b668c0640bacd9dcc8
AriPurnamaAji <aripurnamaaji78@gmail.com>
2020-11-28 19:23:07 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/webService_komok/';
+$config['base_url'] = 'http://localhost/KOMOKProject_2/';
 
 /*
 |--------------------------------------------------------------------------
